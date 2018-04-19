058d14427a682cd1726c7685cf906161abe15324
Bo Peng <pengbo@sraoss.co.jp>
2018-04-19 15:50:38 +0900

    Update buildfarm script.

--- docker-pgpool-II-regression/setup.sh
+++ docker-pgpool-II-regression/setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 case $PGSQL_VERSION in
-	"9.3" ) PGVER=93; JDBC_DRIVER=$JDBC_DRIVER_93 ;; 
-	"9.4" ) PGVER=94; JDBC_DRIVER=$JDBC_DRIVER_94 ;; 
+	"9.5" ) PGVER=95; JDBC_DRIVER=$JDBC_DRIVER_95 ;; 
+	"9.6" ) PGVER=96; JDBC_DRIVER=$JDBC_DRIVER_96 ;; 
+	"10" ) PGVER=10; JDBC_DRIVER=$JDBC_DRIVER_10 ;; 
 	*) echo unknown PGSQL $PGSQL_VERSION ;;
 esac
 
 case $PGPOOL_BRANCH in
-	"master" ) POOLVER=3.5; POOL_REL_RPM=pgpool-II-release-3.5-1.noarch.rpm; ;;
+	"master" ) POOLVER=3.7; POOL_REL_RPM=pgpool-II-release-3.7-1.noarch.rpm; ;;
+	"V3_7_STABLE" ) POOLVER=3.7; POOL_REL_RPM=pgpool-II-release-3.7-1.noarch.rpm; ;;
+	"V3_6_STABLE" ) POOLVER=3.6; POOL_REL_RPM=pgpool-II-release-3.6-1.noarch.rpm; ;;
 	"V3_5_STABLE" ) POOLVER=3.5; POOL_REL_RPM=pgpool-II-release-3.5-1.noarch.rpm; ;;
 	"V3_4_STABLE" ) POOLVER=3.4; POOL_REL_RPM=pgpool-II-release-3.4-1.noarch.rpm; ;;
 	"V3_3_STABLE" ) POOLVER=3.3; POOL_REL_RPM=pgpool-II-release-3.3-1.noarch.rpm; ;;
